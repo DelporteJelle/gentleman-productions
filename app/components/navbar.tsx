d748c3bb216fc84f3a248221cd79e2f07fac8c2c
JelleDelporte <101646425+DelporteJelle@users.noreply.github.com>
2024-06-16 10:16:33 +0200

    main page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,28 +1,36 @@
 import styles from "./navbar.module.css";
 import Image from "next/image";
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsFacebook, BsTwitterX } from "react-icons/bs";
 
 export default function NavBar() {
   return (
     <div className={styles.navbar}>
-      <div className="logo">
+      <div className={styles.logo}>
         <Image
           src="/GP-name.svg"
           alt="/home/"
           width={200}
-          height={50}
+          height={45}
           priority
         />
       </div>
-      <div className="navigation">
+      <div className={styles.navigation}>
         <a href="/">Home</a>
 
         <a href="/about/">About</a>
 
         <a href="/pictures/">Pictures</a>
       </div>
-      <div className="socials">
-        <BsInstagram color="white" />
+      <div className={styles.socials}>
+        <a href="https://www.instagram.com/gentlemanproductions_official">
+          <BsInstagram color="white" className="m-1" />
+        </a>
+        <a>
+          <BsFacebook color="white" className="m-1" />
+        </a>
+        <a>
+          <BsTwitterX color="white" className="m-1" />
+        </a>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,5 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Image
-        src="/GP-logo.svg"
-        alt="/home/"
-        width={300}
-        height={300}
-        priority
-      />
-    </main>
-  );
+  return <main className={styles.main}></main>;
 }
