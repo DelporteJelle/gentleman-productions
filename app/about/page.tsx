8a0357a9aaa2ebc5f02b34cf86afe64250912f85
DelporteJelle <jelledelporte@hotmail.com>
2026-05-15 08:50:18 +0200

    about page restyling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,8 +12,8 @@ import {
 } from "@/components/StateScreens/StateScreens";
 import styles from "./page.module.css";
 
-const ABOUT_DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-// const ABOUT_DESCRIPTION = ""
+// const ABOUT_DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+const ABOUT_DESCRIPTION = ""
 export default function About() {
   const { teamMembers, partners, error, loading } = useAbout();
 
